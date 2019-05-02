a0a44d1203806531371d7525e78b5b683b015626
RenjieLin <42975158+RenjieLin@users.noreply.github.com>
2019-05-02 09:05:30 -0400

    Feedback
    Interface of Feedback

---
+++ Feedback.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AgencyApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AgencyApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Feedback : ContentPage
+    {
+        public Feedback()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Clicked(object sender, EventArgs e)
+        {
+            bool isFeedbackEmpty = string.IsNullOrEmpty(entryFeedback.Text);
+            if(isFeedbackEmpty)
+            {
+                DisplayAlert("Fail", "Please Type Your Feedback", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Successful", "Thanks For Your Feedback", "Ok");
+            }
+        }
+    }
+}
